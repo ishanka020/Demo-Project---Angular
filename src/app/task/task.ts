f9ea88d79165057a3a7575c92f97e82c3f5be05a
Ishanka Perera <pererawcpi.20@itfac.mrt.ac.lk>
2025-10-01 09:30:05 +0530

    Add data to the local storage

--- src/app/task/task.ts
+++ src/app/task/task.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-interface TaskArray{
+interface TaskArray {
   text: string;
   completed: boolean;
 }
@@ -9,31 +9,48 @@ interface TaskArray{
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [FormsModule],   
+  imports: [FormsModule],
   templateUrl: './task.html',
-  styleUrls: ['./task.css']
+  styleUrls: ['./task.css'],
 })
-export class Task {
+export class Task implements OnInit {
   tasks: TaskArray[] = [
-    
-    {text:'Learn Angular', completed:false},
-    {text:'Build a project', completed:false},
-    {text:'Practice daily', completed:false},
-  ]
+    // {text:'Learn Angular', completed:false},
+    // {text:'Build a project', completed:false},
+    // {text:'Practice daily', completed:false},
+  ];
   newTask: string = '';
+
+  ngOnInit(): void {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      const savedTasks = localStorage.getItem('tasks');
+      if (savedTasks) {
+        this.tasks = JSON.parse(savedTasks);
+      }
+    }
+  }
+
+  private saveTasks() {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    }
+  }
 
   addTask() {
     if (this.newTask.trim() !== '') {
-      this.tasks.push({text:this.newTask, completed:false});
+      this.tasks.push({ text: this.newTask, completed: false });
       this.newTask = '';
+      this.saveTasks();
     }
   }
 
   removeTask(index: number) {
     this.tasks.splice(index, 1);
+    this.saveTasks();
   }
 
-  toggleTask(index:number){
-    this.tasks[index].completed =! this.tasks[index].completed;
+  toggleTask(index: number) {
+    this.tasks[index].completed = !this.tasks[index].completed;
+    this.saveTasks();
   }
 }
