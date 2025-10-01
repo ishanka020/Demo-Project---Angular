b3ad7ef11f82d2f21c9f2d42de9e3c2cedac4f2d
Ishanka Perera <pererawcpi.20@itfac.mrt.ac.lk>
2025-10-01 08:40:29 +0530

    Add task completed feature

--- src/app/task/task.ts
+++ src/app/task/task.ts
@@ -1,25 +1,39 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+interface TaskArray{
+  text: string;
+  completed: boolean;
+}
+
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [FormsModule],   // ✅ Only FormsModule now
+  imports: [FormsModule],   
   templateUrl: './task.html',
   styleUrls: ['./task.css']
 })
 export class Task {
-  tasks: string[] = ['Learn Angular', 'Build a project', 'Practice daily'];
+  tasks: TaskArray[] = [
+    
+    {text:'Learn Angular', completed:false},
+    {text:'Build a project', completed:false},
+    {text:'Practice daily', completed:false},
+  ]
   newTask: string = '';
 
   addTask() {
     if (this.newTask.trim() !== '') {
-      this.tasks.push(this.newTask);
+      this.tasks.push({text:this.newTask, completed:false});
       this.newTask = '';
     }
   }
 
   removeTask(index: number) {
     this.tasks.splice(index, 1);
+  }
+
+  toggleTask(index:number){
+    this.tasks[index].completed =! this.tasks[index].completed;
   }
 }
